2f03619df17872017b718949da39d21280c15652
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-03-08 14:52:45 -0500

    updating code for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port= 3000
+const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const Fruit = require('./models/fruits.js')
 
