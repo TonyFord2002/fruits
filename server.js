3e5bd19a39f1b472f8e60c176909756f928d78f1
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-03-08 14:57:50 -0500

    updating code for heroku

--- server.js
+++ server.js
@@ -91,6 +91,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo')
 })
-app.listen(port, () => {
-    console.log('listening on port: ' + port)
+app.listen(PORT, () => {
+    console.log('Making the money on port: ' + PORT)
 })
